e692d8fbe7bcbceccb26e583dd73209ca2842f6a
Georgiy Sulimenko <georgiy.sulimenko@cgi.com>
2022-12-06 18:56:18 +0100

    Lesson 1

--- lesson1/task3.py
+++ lesson1/task3.py
@@ -2,7 +2,7 @@ percent = int(input('Введите число процентов: '))
 
 if (percent % 10 == 1) or (percent == 1):
     word = 'процент'
-elif 1 <= percent <= 4:
+elif 1 < percent <= 4:
     word = 'процента'
 elif (12 <= percent <= 14) or (12 <= percent % 100 <= 14):
     word = 'процентов'
